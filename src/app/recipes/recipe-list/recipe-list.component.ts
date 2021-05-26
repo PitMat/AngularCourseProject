02de748e98d4184260bb24d01186d51b2e021ae6
Mateusz Pitek <68292996+PitMat@users.noreply.github.com>
2021-05-26 12:35:51 +0200

    Recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply description', 'https://embed.widencdn.net/img/beef/tpfhvjvdnh/1120x630px/rancher-recipe-summertime-beef-meatloaf-horizontal.tif?keep=c&u=nvwl20'),
+    new Recipe('A Test Recipe', 'This is simply description', 'https://embed.widencdn.net/img/beef/tpfhvjvdnh/1120x630px/rancher-recipe-summertime-beef-meatloaf-horizontal.tif?keep=c&u=nvwl20'),
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
+
